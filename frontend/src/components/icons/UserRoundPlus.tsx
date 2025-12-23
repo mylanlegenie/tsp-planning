2fdd6c217994e7d4055b3a1409f70eeaafcf7f3b
Anais HACHEMANE <anais.hachemane@essec.edu>
2025-12-23 21:56:46 +0100

    feat: add HistoryIcon, UserRoundPlus, CalendarIcon, CalendarCheckIocn and StatisticalIcon + add tailwindMerge and motion

---
+++ frontend/src/components/icons/UserRoundPlus.tsx
@@ -0,0 +1,124 @@
+"use client";
+import type { Variants } from "motion/react";
+import type { HTMLAttributes, MouseEvent } from "react";
+import { forwardRef, useCallback, useImperativeHandle, useRef } from "react";
+import { motion, useAnimation } from "motion/react";
+import { twMerge } from "tailwind-merge";
+export interface UserRoundPlusIconHandle {
+  startAnimation: () => void;
+  stopAnimation: () => void;
+}
+
+interface UserRoundPlusIconProps extends HTMLAttributes<HTMLDivElement> {
+  size?: number;
+}
+
+const VERTICAL_BAR_VARIANTS: Variants = {
+  normal: {
+    opacity: 1,
+  },
+  animate: {
+    opacity: [0, 1],
+    pathLength: [0, 1],
+    transition: {
+      delay: 0.3,
+      duration: 0.2,
+      opacity: { duration: 0.1, delay: 0.3 },
+    },
+  },
+};
+
+const HORIZONTAL_BAR_VARIANTS: Variants = {
+  normal: {
+    opacity: 1,
+  },
+  animate: {
+    opacity: [0, 1],
+    pathLength: [0, 1],
+    transition: {
+      delay: 0.6,
+      duration: 0.2,
+      opacity: { duration: 0.1, delay: 0.6 },
+    },
+  },
+};
+
+const UserRoundPlusIcon = forwardRef<
+  UserRoundPlusIconHandle,
+  UserRoundPlusIconProps
+>(({ onMouseEnter, onMouseLeave, className, size = 28, ...props }, ref) => {
+  const controls = useAnimation();
+  const isControlledRef = useRef(false);
+
+  useImperativeHandle(ref, () => {
+    isControlledRef.current = true;
+
+    return {
+      startAnimation: () => controls.start("animate"),
+      stopAnimation: () => controls.start("normal"),
+    };
+  });
+
+  const handleMouseEnter = useCallback(
+    (e: MouseEvent<HTMLDivElement>) => {
+      if (!isControlledRef.current) {
+        controls.start("animate");
+      } else {
+        onMouseEnter?.(e);
+      }
+    },
+    [controls, onMouseEnter]
+  );
+
+  const handleMouseLeave = useCallback(
+    (e: MouseEvent<HTMLDivElement>) => {
+      if (!isControlledRef.current) {
+        controls.start("normal");
+      } else {
+        onMouseLeave?.(e);
+      }
+    },
+    [controls, onMouseLeave]
+  );
+
+  return (
+    <div
+      className={twMerge(className)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      {...props}
+    >
+      <motion.svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        initial="normal"
+      >
+        <path d="M2 21a8 8 0 0 1 13.292-6" />
+        <circle cx="10" cy="8" r="5" />
+        <motion.path
+          d="M19 16v6"
+          variants={VERTICAL_BAR_VARIANTS}
+          initial="normal"
+          animate={controls}
+        />
+        <motion.path
+          d="M22 19h-6"
+          variants={HORIZONTAL_BAR_VARIANTS}
+          initial="normal"
+          animate={controls}
+        />
+      </motion.svg>
+    </div>
+  );
+});
+
+UserRoundPlusIcon.displayName = "UserRoundPlusIcon";
+
+export { UserRoundPlusIcon };
