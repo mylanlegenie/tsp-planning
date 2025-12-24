8258516f9941e9b92026f347ecbcb0a0d9d546f4
Anais HACHEMANE <anais.hachemane@essec.edu>
2025-12-24 21:41:14 +0100

    feat: add button in MissionCard

--- frontend/src/components/molecules/MissionCard.tsx
+++ frontend/src/components/molecules/MissionCard.tsx
@@ -28,6 +28,27 @@ function MissionCard({ listeMission }: { listeMission: Mission }) {
             </div>
           ))}
         </div>
+        <div className="border border-gray-200 border-0.5 rounded-xs">
+          <div className="px-2 py-1">
+            <button className="bg-green-600 w-full p-1 hover:bg-green-500 transition-colors cursor-pointer">
+              <span className="text-white font-bold text-center">
+                Prendre en charge
+              </span>
+            </button>
+          </div>
+          <div className="flex items-center justify-around px-2 py-1 gap-5">
+            <button className="bg-amber-500 flex-1 p-1 cursor-pointer">
+              <span className="text-white font-semibold text-center">
+                Modifier
+              </span>
+            </button>
+            <button className="bg-blue-800 flex-1 p-1 cursor-pointer">
+              <span className="text-white font-semibold text-center">
+                Afficher
+              </span>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
